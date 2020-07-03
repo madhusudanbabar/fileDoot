a4b5c385686ebe5ed219db963fa3d121f1524057
Krypton <madhusudanbabar@gmail.com>
2020-07-03 16:18:59 +0530

    header

--- src/renderer/components/header.vue
+++ src/renderer/components/header.vue
@@ -1,62 +1,24 @@
 <template>
-  <header class="main-header">
-    <nav>
-      <div>
-        <nuxt-link
-          to="/"
-          exact
-        >
-          System info
-        </nuxt-link>
-      </div>
-      <div>
-        <nuxt-link
-          to="/other"
-          exact
-        >
-          Next page
-        </nuxt-link>
-      </div>
-    </nav>
-  </header>
+    <v-toolbar
+      dark
+      prominent
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>FileDoot</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
 </template>
 
-<script>
-export default {
-
-}
+<script lang="ts">
+export default {};
 </script>
 
-<style scoped>
-    .main-header {
-        height: 50px;
-        display: flex;
-        background-color: #364758;
-    }
-
-    nav{
-        width: 100%;
-        display: flex;
-        justify-content: center;
-    }
-
-    nav div{
-        height: 100%;
-        margin: 0 20px;
-        display: flex;
-        align-items: center;
-    }
-
-    a {
-        font-family: Helvetica, sans-serif;
-        color: white;
-        font-size: 1rem;
-        font-weight: 100;
-        text-decoration: underline;
-        text-transform: uppercase;
-    }
-
-    a:hover{
-        opacity: 0.9;
-    }
+<style>
 </style>
